d6048bc97080d81c50eda1a7aff957ff7672206e
vladproduction <vladbogdantsev@gmail.com>
2024-02-16 12:04:16 +0200

    test //todo

--- data-processor-db-app/src/test/java/com/vladproduction/dataprocessordbapp/DataProcessorDbAppApplicationTests.java
+++ data-processor-db-app/src/test/java/com/vladproduction/dataprocessordbapp/DataProcessorDbAppApplicationTests.java
@@ -1,13 +1,22 @@
 package com.vladproduction.dataprocessordbapp;
 
+import com.vladproduction.dataprocessordbapp.controller.ProducerController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DataProcessorDbAppApplicationTests {
 
+    @Autowired
+    private ProducerController producerController;
+    @Autowired
+    private TestRestTemplate testRestTemplate;
     @Test
     void contextLoads() {
+        //todo
+        //testRestTemplate.postForEntity("/api/producer", )
     }
 
 }
