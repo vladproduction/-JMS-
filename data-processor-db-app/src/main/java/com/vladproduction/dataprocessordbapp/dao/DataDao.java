71ea40db6f49276dd12995c6ac7765290454a088
vladproduction <vladbogdantsev@gmail.com>
2024-02-19 17:57:29 +0200

    test add; h2 DB as testing Data add

--- data-processor-db-app/src/main/java/com/vladproduction/dataprocessordbapp/dao/DataDao.java
+++ data-processor-db-app/src/main/java/com/vladproduction/dataprocessordbapp/dao/DataDao.java
@@ -2,7 +2,12 @@ package com.vladproduction.dataprocessordbapp.dao;
 
 import com.vladproduction.dataprocessordbapp.model.Data;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @Repository
 public class DataDao {
@@ -16,6 +21,41 @@ public class DataDao {
     public void saveData(Data data){
         jdbcTemplate.update("insert into data (name, type, isTrue) values(?, ?, ?)",
                 data.getName(), data.getType(), data.isTrue() ? 1:0);
+    }
+
+    public int countAllData(){
+        return jdbcTemplate.queryForObject("select count(*) from data", Integer.class);
+    }
+
+    public void createTable(){
+        String sql = "create table if not exists data\n" +
+                "(\n" +
+                "    id     int auto_increment\n" +
+                "        primary key,\n" +
+                "    name   varchar(45) null,\n" +
+                "    type   int         null,\n" +
+                "    isTrue tinyint     null\n" +
+                ");";
+        jdbcTemplate.execute(sql);
+    }
+
+    public void clearTable(){
+        String sql = "delete from data";
+        jdbcTemplate.execute(sql);
+    }
+
+    public List<Data> findAll(){
+        String sql = "select * from data";
+        return jdbcTemplate.query(sql, new RowMapper<Data>() {
+            @Override
+            public Data mapRow(ResultSet rs, int rowNum) throws SQLException {
+                String name = rs.getString("name");
+                int type = rs.getInt("type");
+                boolean isTrue = rs.getBoolean("isTrue");
+                Data data = new Data(name, type, isTrue);
+                return data;
+            }
+        });
     }
 
 
--- data-processor-db-app/src/test/java/com/vladproduction/dataprocessordbapp/DataProcessorDbAppApplicationTests.java
+++ data-processor-db-app/src/test/java/com/vladproduction/dataprocessordbapp/DataProcessorDbAppApplicationTests.java
@@ -1,22 +1,57 @@
 package com.vladproduction.dataprocessordbapp;
 
-import com.vladproduction.dataprocessordbapp.controller.ProducerController;
+import com.vladproduction.dataprocessordbapp.dao.DataDao;
+import com.vladproduction.dataprocessordbapp.model.Data;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DataProcessorDbAppApplicationTests {
 
-    @Autowired
-    private ProducerController producerController;
+
     @Autowired
     private TestRestTemplate testRestTemplate;
+    @Autowired
+    DataDao dataDao;
+
+    @BeforeEach
+    public void init(){
+        dataDao.createTable();
+        dataDao.clearTable();
+    }
     @Test
-    void contextLoads() {
-        //todo
-        //testRestTemplate.postForEntity("/api/producer", )
+    void testTrueData() throws InterruptedException {
+        Data data = new Data("data1-h2", 100, true);
+        verifyData(data);
+    }
+
+    @Test
+    void testFalseData() throws InterruptedException {
+        Data data = new Data("data2", 150, false);
+        verifyData(data);
+    }
+
+
+    private void verifyData(Data data) throws InterruptedException {
+        int beforeSent = dataDao.countAllData();
+        ResponseEntity<Void> voidResponseEntity1 = testRestTemplate.postForEntity("/api/producer", data, Void.class);
+
+        Assertions.assertTrue(voidResponseEntity1.getStatusCode().value() == 200);
+        //todo --> embeeded adjustice  and thread.sleep
+        Thread.sleep(1000);
+        int afterSent = dataDao.countAllData();
+        Assertions.assertTrue(beforeSent + 1 == afterSent);
+        List<Data> dataList = dataDao.findAll();
+        Assertions.assertTrue(dataList.size() == 1);
+        Data actualData = dataList.get(0);
+        Assertions.assertEquals(data, actualData);
     }
 
 }
